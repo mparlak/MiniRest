41387eb4a877992b4b7c83336b53eb490edfc313
Muhammet Parlak <mparlak@boynergrup.com>
2019-02-28 11:47:28 +0300

    some refactor.

--- src/MiniRest.Net45/Properties/AssemblyInfo.cs
+++ src/MiniRest.Net45/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MiniRest")]
+[assembly: AssemblyTitle("MiniRest.Net45")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MiniRest")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("MiniRest.Net45")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("73fffa4f-06f2-478a-ae36-44b8d2a44f28")]
+[assembly: Guid("7507dd1b-49fd-44e7-a957-3299c4e94608")]
 
 // Version information for an assembly consists of the following four values:
 //
